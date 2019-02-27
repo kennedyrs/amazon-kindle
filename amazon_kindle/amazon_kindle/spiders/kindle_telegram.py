501a3f0cca9497918289ef648d79425869c7b342
Kennedy Rodrigues <kennedyrodrigues@outlook.com>
2019-02-26 20:26:17 -0400

    Spider criada e configurada

--- amazon_kindle/amazon_kindle/spiders/kindle_telegram.py
+++ amazon_kindle/amazon_kindle/spiders/kindle_telegram.py
@@ -5,7 +5,8 @@ import scrapy
 class KindleTelegramSpider(scrapy.Spider):
     name = 'kindle_telegram'
     allowed_domains = ['https://www.amazon.com.br/']
-    start_urls = ['http://https://www.amazon.com.br//']
+    start_urls = ['https://www.amazon.com.br/gp/product/B0773XBMB6/']
 
     def parse(self, response):
-        pass
+        name = response.xpath('//span[@id="priceblock_ourprice"]/text()').get()
+        self.log('O Preço do Kindle Papperwhite %s' % name)
